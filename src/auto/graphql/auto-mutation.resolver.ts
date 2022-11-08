adbfe3705317b2c9270e4029449b2c1b5ebda945
Eric Wollmann <Eric-woll@web.de>
2022-11-08 17:13:50 +0100

    graphql

---
+++ src/auto/graphql/auto-mutation.resolver.ts
@@ -0,0 +1,128 @@
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { type CreateError, type UpdateError } from '../service/errors.js';
+import { UseGuards, UseInterceptors } from '@nestjs/common';
+import { type Auto } from '../entity/auto.entity.js';
+import { AutoWriteService } from '../service/auto-write.service.js';
+import { type IdInput } from './auto-query.resolver.js';
+import { JwtAuthGraphQlGuard } from '../../security/auth/jwt/jwt-auth-graphql.guard.js';
+import { ResponseTimeInterceptor } from '../../logger/response-time.interceptor.js';
+import { Roles } from '../../security/auth/roles/roles.decorator.js';
+import { RolesGraphQlGuard } from '../../security/auth/roles/roles-graphql.guard.js';
+import { type Fahrzeugklasse } from '../entity/fahrzeugklasse.entity.js';
+import { UserInputError } from 'apollo-server-express';
+import { getLogger } from '../../logger/logger.js';
+
+type AutoCreateDTO = Omit<
+    Auto,
+    'aktualisiert' | 'erzeugt' | 'id' | 'fahrzeugklassen' | 'version'
+> & { fahrzeugklassen: string[] };
+type AutoUpdateDTO = Omit<Auto, 'aktualisiert' | 'erzeugt' | 'fahrzeugklassen'>;
+
+@Resolver()
+@UseGuards(JwtAuthGraphQlGuard, RolesGraphQlGuard)
+@UseInterceptors(ResponseTimeInterceptor)
+export class AutoMutationResolver {
+    readonly #service: AutoWriteService;
+
+    readonly #logger = getLogger(AutoMutationResolver.name);
+
+    constructor(service: AutoWriteService) {
+        this.#service = service;
+    }
+
+    @Mutation()
+    @Roles('admin', 'mitarbeiter')
+    async create(@Args('input') autoDTO: AutoCreateDTO) {
+        this.#logger.debug('create: autoDTO=%o', autoDTO);
+
+        const result = await this.#service.create(this.#dtoToAuto(autoDTO));
+        this.#logger.debug('createAuto: result=%o', result);
+
+        if (Object.prototype.hasOwnProperty.call(result, 'type')) {
+            // UserInputError liefert Statuscode 200
+            throw new UserInputError(
+                this.#errorMsgCreateAuto(result as CreateError),
+            );
+        }
+        return result;
+    }
+
+    @Mutation()
+    @Roles('admin', 'mitarbeiter')
+    async update(@Args('input') auto: AutoUpdateDTO) {
+        this.#logger.debug('update: buch=%o', auto);
+        const versionStr = `"${auto.version?.toString()}"`;
+
+        const result = await this.#service.update(
+            auto.id,
+            auto as Auto,
+            versionStr,
+        );
+        if (typeof result === 'object') {
+            throw new UserInputError(this.#errorMsgUpdateAuto(result));
+        }
+        this.#logger.debug('updateAuto: result=%d', result);
+        return result;
+    }
+
+    @Mutation()
+    @Roles('admin')
+    async delete(@Args() id: IdInput) {
+        const idStr = id.id;
+        this.#logger.debug('delete: id=%s', idStr);
+        const result = await this.#service.delete(idStr);
+        this.#logger.debug('deleteAuto: result=%s', result);
+        return result;
+    }
+
+    #dtoToAuto(autoDTO: AutoCreateDTO): Auto {
+        const auto: Auto = {
+            id: undefined,
+            version: undefined,
+            marke: autoDTO.marke,
+            typ: autoDTO.typ,
+            preis: autoDTO.preis,
+            lieferbar: autoDTO.lieferbar,
+            rabatt: autoDTO.rabatt,
+            datum: autoDTO.datum,
+            fahrzeugklassen: [],
+            erzeugt: undefined,
+            aktualisiert: undefined,
+        };
+
+        autoDTO.fahrzeugklassen.forEach((s) => {
+            const fahrzeugklasse: Fahrzeugklasse = {
+                id: undefined,
+                fahrzeugklasse: s,
+                auto,
+            };
+            auto.fahrzeugklassen.push(fahrzeugklasse);
+        });
+
+        return auto;
+    }
+
+    #errorMsgCreateAuto(err: CreateError) {
+        switch (err.type) {
+            case 'ConstraintViolations':
+                return err.messages.join(' ');
+            default:
+                return 'Unbekannter Fehler';
+        }
+    }
+
+    #errorMsgUpdateAuto(err: UpdateError) {
+        switch (err.type) {
+            case 'ConstraintViolations':
+                return err.messages.join(' ');
+            case 'AutoNotExists':
+                return `Es gibt kein Buch mit der ID ${err.id}`;
+            case 'VersionInvalid':
+                return `"${err.version}" ist keine gueltige Versionsnummer`;
+            case 'VersionOutdated':
+                return `Die Versionsnummer "${err.version}" ist nicht mehr aktuell`;
+            default:
+                return 'Unbekannter Fehler';
+        }
+    }
+}
---
+++ src/auto/graphql/auto-query.resolver.ts
@@ -0,0 +1,78 @@
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import { type Auto } from '../entity/auto.entity.js';
+import { AutoReadService } from '../service/auto-read.service.js';
+import { ResponseTimeInterceptor } from '../../logger/response-time.interceptor.js';
+import { UseInterceptors } from '@nestjs/common';
+import { UserInputError } from 'apollo-server-express';
+import { getLogger } from '../../logger/logger.js';
+
+export type AutoDTO = Omit<
+    Auto,
+    'aktualisiert' | 'erzeugt' | 'fahrzeugklassen'
+> & { fahrzeugklassen: string[] };
+export interface IdInput {
+    id: string;
+}
+
+@Resolver()
+@UseInterceptors(ResponseTimeInterceptor)
+export class AutoQueryResolver {
+    readonly #service: AutoReadService;
+
+    readonly #logger = getLogger(AutoQueryResolver.name);
+
+    constructor(service: AutoReadService) {
+        this.#service = service;
+    }
+
+    @Query('auto')
+    async findById(@Args() id: IdInput) {
+        const idStr = id.id;
+        this.#logger.debug('findById: id=%s', idStr);
+
+        const auto = await this.#service.findById(idStr);
+        if (auto === undefined) {
+            // UserInputError liefert Statuscode 200
+            throw new UserInputError(
+                `Es wurde kein Auto mit der ID ${idStr} gefunden.`,
+            );
+        }
+        const autoDTO = this.#toAutoDTO(auto);
+        this.#logger.debug('findById: autoDTO=%o', autoDTO);
+        return autoDTO;
+    }
+
+    @Query('autos')
+    async find(@Args() marke : { marke: string } | undefined) {
+        const markeStr = marke?.marke;
+        this.#logger.debug('find: marke=%s', markeStr);
+        const suchkriterium = markeStr === undefined ? {} : { marke: markeStr };
+        const autos = await this.#service.find(suchkriterium);
+        if (autos.length === 0) {
+            // UserInputError liefert Statuscode 200
+            throw new UserInputError('Es wurden keine Autos gefunden.');
+        }
+
+        const autosDTO = autos.map((auto) => this.#toAutoDTO(auto));
+        this.#logger.debug('find: autosDTO=%o', autosDTO);
+        return autosDTO;
+    }
+
+    #toAutoDTO(auto: Auto) {
+        const fahrzeugklassen = auto.fahrzeugklassen.map(
+            (fahrzeugklasse) => fahrzeugklasse.fahrzeugklasse!, // eslint-disable-line @typescript-eslint/no-non-null-assertion
+        );
+        const autoDTO: AutoDTO = {
+            id: auto.id,
+            version: auto.version,
+            marke: auto.marke,
+            typ: auto.typ,
+            preis: auto.preis,
+            lieferbar: auto.lieferbar,
+            rabatt: auto.rabatt,
+            datum: auto.datum,
+            fahrzeugklassen,
+        };
+        return autoDTO;
+    }
+}
