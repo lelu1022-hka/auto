e554bd8fbf115fe6d4c62950e501e23345abc9db
Luca Leitzbach <lelu1022@h-ka.de>
2022-11-09 16:03:23 +0100

    eslint-disabled

--- src/config/dev/testdaten.ts
+++ src/config/dev/testdaten.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
 import { type Auto } from '../../auto/entity/auto.entity.js';
 import { type Fahrzeugklasse } from './../../auto/entity/fahrzeugklasse.entity.js';
 
